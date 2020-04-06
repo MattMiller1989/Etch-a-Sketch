55013ff83cfd42bbc2dae528f1ae0709138954d9
MattMiller1989 <57654274+MattMiller1989@users.noreply.github.com>
2020-04-06 10:35:22 -0500

    Update Etch-a-Sketch.js

--- Etch-a-Sketch.js
+++ Etch-a-Sketch.js
@@ -1,14 +1,23 @@
-const container=document.querySelector('#grid-container');
 
-for(let x=0;x<16;x++){
-    for(let y=0;y<16;y++){
-        let currDiv=document.createElement('div');
-        currDiv.classList.add('grid-square');
-        container.appendChild(currDiv);
-        
-        currDiv.addEventListener("mouseover", function( event ){
-            event.target.style.backgroundColor="black";
-        });
+
+makeGrid(16);
+function makeGrid(gridSize){
+    const container=document.querySelector('#grid-container');
+    container.style.gridTemplateRows=`repeat(${gridSize},1fr)`;
+    container.style.gridTemplateColumns=`repeat(${gridSize},1fr)`;    
+    
+    for(let x=0;x<gridSize;x++){
+        for(let y=0;y<gridSize;y++){
+            let currDiv=document.createElement('div');
+            currDiv.classList.add('grid-square');
+            container.appendChild(currDiv);
+
+            
+            
+            currDiv.addEventListener("mouseover", function( event ){
+                event.target.style.backgroundColor="black";
+            });
+        }
     }
 }
 
@@ -19,4 +28,13 @@ function resetPixels(){
          currDivs[x].style.backgroundColor="white";
      }
     
+}
+function resizeGrid(){
+    var size= prompt("Please enter the size you desire","Please enter a number");
+    if(isNaN(size)){
+        resizeGrid();
+    }
+    resetPixels();
+    makeGrid(size);
+
 }
